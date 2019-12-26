0911b23573dd75cce8d4b271c67caa6df12b1f40
lavaleta <aminic17@raf.rs>
2019-12-26 23:15:12 +0100

    Initial commit

---
+++ src/main/java/mojagrupicaza/mingdji/MingdjiApplication.java
@@ -0,0 +1,13 @@
+package mojagrupicaza.mingdji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingdjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingdjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mojagrupicaza/mingdji/MingdjiApplicationTests.java
@@ -0,0 +1,13 @@
+package mojagrupicaza.mingdji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingdjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
